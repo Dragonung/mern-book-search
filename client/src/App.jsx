6ba7179ea7c5860fec59fbd4dae8877f1d39a63e
Daniel Chen <danielchenishere@gmail.com>
2024-11-30 13:42:32 -0800

    App.jsx: added Apollo & refactor code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+
+//main GraphQL API endpoint
+const authLink = setContext((__, { headers }) => {
+  //get auth token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token
+        ? `Bearer ${token}`
+        : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Outlet />
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
